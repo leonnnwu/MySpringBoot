e913dbb5c62b885e33284da3e90e3ab406da774d
lwu <liang_wu@intuit.com>
2016-09-02 07:56:33 -0700

    Disable specific auto-configuration.

--- src/main/java/com/lwu/boot/Application.java
+++ src/main/java/com/lwu/boot/Application.java
@@ -2,11 +2,12 @@ package com.lwu.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class Application {
 
     @RequestMapping("/")
